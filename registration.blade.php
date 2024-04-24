df89ba234536216287adc340b8981c10e6859d26
Hamza Khalid <103095753+ranahamzakhalid@users.noreply.github.com>
2024-04-24 12:12:53 +0500

    Add files via upload

---
+++ registration.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Registration</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4" style= "margin-top: 50px;">
+                <h3>Registration</h3>
+                <hr>
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{$error}}</li>
+                    @endforeach
+                </ul>
+                <form action="{{route('register.user')}}" method="POST">
+                    @if (Session::has('success'))
+                        <div class="alert alert-success">{{session::get('success')}}</div>
+                    @endif
+                    @if (Session::has('fail'))
+                        <div class="alert alert-danger">{{session::get('fail')}}</div>
+                    @endif
+                    @csrf
+                    <div class="form-group">
+                        <label for="name">Full Name</label>
+                        <input type="text" class="form-contol" placeholder="Enter Name" name="name" value="{{old('name')}}">    
+                        <span class="text-danger">@error('name') {{$message}} @enderror</span>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Enter Email</label>
+                        <input type="text" class="form-contol" placeholder="Enter Email" name="email" value="{{old('email')}}">    
+                        <span class="text-danger">@error('email') {{$message}} @enderror</span>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" class="form-contol" placeholder="Enter Password" name="password" value="{{old('password')}}">    
+                        <span class="text-danger">@error('password') {{$message}} @enderror</span>
+                    </div>
+                    <button class="btn btn-block btn-primary">Registration</button>
+                    <br>
+                    <a href="login">Already Rergistered !! Login Here</a>
+                </form> 
+            </div>
+        </div>
+    </div>
+</body>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+</html>
